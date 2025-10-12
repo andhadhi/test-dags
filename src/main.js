3dfc694bfcba1a6e5f7b530b1dd256d625acc76d
fizan2904 <fizan.nagarchi6@gmail.com>
2025-10-12 18:03:57 +0530

    init

---
+++ src/main.js
@@ -0,0 +1,8 @@
+const test = require('./test');
+
+function main() {
+    console.log("Hello, World!");
+    test.print();
+}
+
+module.exports = { main };
---
+++ src/test.js
@@ -0,0 +1,5 @@
+function print() {
+    console.log("This is a test file.");
+}
+
+module.exports = { print };
